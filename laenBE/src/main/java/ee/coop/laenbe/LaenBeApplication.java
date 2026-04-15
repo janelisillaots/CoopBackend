e76fe06f0975fd579fd3731657de54c9e4928e8f
janelisillaots <janelisillaots@gmail.com>
2026-04-15 19:20:26 +0300

    initial files

---
+++ laenBE/src/main/java/ee/coop/laenbe/LaenBeApplication.java
@@ -0,0 +1,13 @@
+package ee.coop.laenbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaenBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaenBeApplication.class, args);
+    }
+
+}
---
+++ laenBE/src/test/java/ee/coop/laenbe/LaenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.coop.laenbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaenBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
